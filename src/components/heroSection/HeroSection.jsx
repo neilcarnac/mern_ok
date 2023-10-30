cf69d2ef75a40b559d229fc583601f9c46f05ffb
neilo <neil.carnac@somaiya.edu>
2023-10-31 02:52:00 +0530

    -added relatable products on product info page

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -10,7 +10,7 @@ function HeroSection() {
     className="w-full h-full object-cover"
   />
   <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
-  <div className="absolute top-1/4 ml-40 transform -translate-y-1/2">
+  <div className="absolute top-1/4 ml-40 pt-40 transform -translate-y-1/2">
     <h1 className="text-white text-5xl font-bold mb-4">
       Explore the World of Beverages
     </h1>
@@ -19,7 +19,7 @@ function HeroSection() {
     </p>
   <Link to="/product-card"> {/* This Link component will navigate to /product-card */}
     <button className="bg-black text-white px-4 py-2 rounded-lg mt-4 hover:bg-blue-600 ease-in transition-[2000ms]">
-      Go to Product Card
+      Buy Now
     </button>
   </Link>
   </div>
